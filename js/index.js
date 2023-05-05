ef6e55318c88cf4ed623c3815ed00b588e4d8ea8
Karim-124 <ka1465@fayoum.edu.eg>
2023-05-05 20:49:01 +0300

    Jquery plugins

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$(".skitter-large").skitter({interval:1000 , auto_play:false});
+
+
+	$(".owl-carousel").owlCarousel({margin:30 , items:2});
+  });
+
+
+  var typed = new Typed('.element', {
+	strings: ["Develop Mobile Apps.", "Develop Web Apps."],
+	typeSpeed: 100,
+	backSpeed: 50,
+	showCursor :false
+  });
+  
+  
